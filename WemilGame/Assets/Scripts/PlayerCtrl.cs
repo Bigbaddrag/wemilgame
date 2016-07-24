4b97b407c77e208eb89bb0811cf9bfc0e283d4d2
Bigbaddrag <sephirothpw@hotmail.com>
2016-07-23 22:21:44 -0500

    it's a sign
    there's a sign now. It doesn't do much.
    Also comments in the code yay

--- WemilGame/Assets/Scripts/PlayerCtrl.cs
+++ WemilGame/Assets/Scripts/PlayerCtrl.cs
@@ -7,8 +7,9 @@ public class PlayerCtrl : MonoBehaviour {
 	public float jumpForce = 600.0f;
 	
 	private Vector2 moveDir;
-	//private Animator anim;
+	//private Animator anim;	// This anim object is used to reference the object's animator, which is unused for now
 	private Rigidbody2D rb;
+
 	private Vector3 minScreenBounds;
 	private Vector3 maxScreenBounds;
 
@@ -17,29 +18,32 @@ public class PlayerCtrl : MonoBehaviour {
 		moveDir = Vector2.zero;
 		//anim = GetComponent<Animator> ();
 		rb = GetComponent<Rigidbody2D> ();
-
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
-		
+		// Input.GetAxisRaw detects A and D keys (also left/right arrow keys) and gives a float value
+		// Which we put in a vector (like physics kind of vector)
+		// Normalize is important, it sets the vectors magnitude to 1, otherwise the player object would fly off into the distance
 		float moveX = Input.GetAxisRaw ("Horizontal");
 		moveDir = new Vector2 (moveX, 0);
 		moveDir.Normalize ();
 		//anim.SetFloat("Speed", Mathf.Abs(moveX) );
 		
 		// walk
+		// transform holds info like position, rotation, scale
 		transform.position += new Vector3 (moveDir.x, moveDir.y, 0.0f) * movespeed * Time.deltaTime;	//speed * direction * time
-		
+
+		// this sections just makes it so the object can't move outside the left or right edges of the camera
 		minScreenBounds = Camera.main.ScreenToWorldPoint(new Vector3(0, 0, 0));
 		maxScreenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
-		
 		transform.position = new Vector3(
 			Mathf.Clamp(transform.position.x, minScreenBounds.x + 0.5f, maxScreenBounds.x - 0.5f),
 			transform.position.y, 
 			transform.position.z);
-		
+
+		// Flip the sprite based on the direction it is going
 		if (moveX > 0) {
 			transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
 		}
@@ -48,9 +52,21 @@ public class PlayerCtrl : MonoBehaviour {
 		}
 		
 		// jump
+		// Also make sure the object isn't falling, or already jumping
+		// We check based on 0.01 instead of looking for zero to allow for some room for small miniscule changes
+		// This may need to be tweaked for allowing jumps while going up or down slopes
 		if (rb.velocity.y >= -0.01f && rb.velocity.y <= 0.01f && Input.GetKeyDown(KeyCode.Space)) {
 			rb.AddForce(new Vector2(0, jumpForce));
 		}
 		//anim.SetFloat("vSpeed", rb.velocity.y);
+	}
+
+	// Colliders that are attached to objects can be set as triggers, which don't act like walls. They can be passed through
+	// This function is called when this object enters a trigger box attached to another object
+	void OnTriggerEnter2D (Collider2D col) {
+		if (col.tag == "Interactable") {
+			print ("interact?");
+
+		}
 	}
 }
---
+++ WemilGame/Assets/Scripts/sign1Ctrl.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class sign1Ctrl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	// This sign will react when the player walks next to it
+	// Can be used as template code for displaying text or whatever
+	void OnTriggerEnter2D (Collider2D col) {
+		if (col.tag == "Player") {
+			// It just prints this to Unity's debug console for now
+			print ("Don't touch me");
+			
+		}
+	}
+}
