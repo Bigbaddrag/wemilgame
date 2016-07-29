c71fd99134626c012abb2e7368e60ac07bbfbb26
Bigbaddrag <sephirothpw@hotmail.com>
2016-07-28 20:08:43 -0500

    pointer
    Player box has an arrow that points in the direction it's moving now.
    Placeholder for animation.
    
    also the sign shows text when you're next to it, but it's still kinda
    wonky

--- WemilGame/Assets/Scripts/sign1Ctrl.cs
+++ WemilGame/Assets/Scripts/sign1Ctrl.cs
@@ -3,23 +3,51 @@ using System.Collections;
 
 public class sign1Ctrl : MonoBehaviour {
 
+	private Transform plyrTarget;
+	private bool talk;
+	private GUIText gt;
+
 	// Use this for initialization
 	void Start () {
-	
+		gt = GetComponent<GUIText> ();
+		talk = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		plyrTarget = GameObject.FindGameObjectWithTag ("Player").transform;
 	}
 
-	// This sign will react when the player walks next to it
-	// Can be used as template code for displaying text or whatever
+	void OnGUI ()
+	{
+		if (talk) {
+			Vector3 getPixelPos = Camera.main.WorldToScreenPoint (transform.position);
+			getPixelPos.y = Screen.height - getPixelPos.y;
+
+//			gameObject.renderer.bounds.center;
+//
+//			GetComponent<SpriteRenderer>().bounds.center.x;
+			GUIContent gc = new GUIContent(gt.text);
+			GUIStyle gs = new GUIStyle();
+
+			gs.alignment = gt.anchor;
+
+
+
+			//this ain't flying too well atm
+			GUI.Label (new Rect (getPixelPos.x - 50.0f, getPixelPos.y - 50.0f, 200f, 100f), gc, gs);
+		}
+	}
+	
 	void OnTriggerEnter2D (Collider2D col) {
 		if (col.tag == "Player") {
-			// It just prints this to Unity's debug console for now
-			print ("Don't touch me");
-			
+			talk = true;
+		}
+	}
+
+	void OnTriggerExit2D (Collider2D col){
+		if (col.tag == "Player") {
+			talk = false;
 		}
 	}
 }
