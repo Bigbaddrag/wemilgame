6bd7e8cca3d175472881cc78a9b0c132d775c50f
Bigbaddrag <sephirothpw@hotmail.com>
2016-07-29 20:19:56 -0500

    speech bubbles
    figured out how to do speech bubbles. The sign now has personal space
    issues

--- WemilGame/Assets/Scripts/sign1Ctrl.cs
+++ WemilGame/Assets/Scripts/sign1Ctrl.cs
@@ -1,53 +1,44 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class sign1Ctrl : MonoBehaviour {
 
 	private Transform plyrTarget;
-	private bool talk;
-	private GUIText gt;
+    private Canvas c;
+    private Text t;
+    private float Distance;
+    private bool touched;
+
+    public Texture img;
+    public Texture imgShock;
 
 	// Use this for initialization
 	void Start () {
-		gt = GetComponent<GUIText> ();
-		talk = false;
+        c = GetComponentInChildren<Canvas>();
+        //c.enabled = false;
+        t = GetComponentInChildren<Text>();
+        touched = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		plyrTarget = GameObject.FindGameObjectWithTag ("Player").transform;
-	}
 
-	void OnGUI ()
-	{
-		if (talk) {
-			Vector3 getPixelPos = Camera.main.WorldToScreenPoint (transform.position);
-			getPixelPos.y = Screen.height - getPixelPos.y;
-
-//			gameObject.renderer.bounds.center;
-//
-//			GetComponent<SpriteRenderer>().bounds.center.x;
-			GUIContent gc = new GUIContent(gt.text);
-			GUIStyle gs = new GUIStyle();
-
-			gs.alignment = gt.anchor;
-
-
-
-			//this ain't flying too well atm
-			GUI.Label (new Rect (getPixelPos.x - 50.0f, getPixelPos.y - 50.0f, 200f, 100f), gc, gs);
-		}
+        if (Vector3.Distance(transform.position, plyrTarget.position) > 2.0f && touched)
+        {
+            //c.enabled = false;    // this makes the canvas (text bubble) disappear
+            t.text = "Thank you.";
+            c.GetComponentInChildren<CanvasRenderer>().SetTexture(img);
+        }
 	}
 	
 	void OnTriggerEnter2D (Collider2D col) {
 		if (col.tag == "Player") {
-			talk = true;
-		}
-	}
-
-	void OnTriggerExit2D (Collider2D col){
-		if (col.tag == "Player") {
-			talk = false;
+            c.enabled = true;
+            c.GetComponentInChildren<CanvasRenderer>().SetTexture(imgShock);
+            t.text = "DON'T TOUCH ME!";
+            touched = true;
 		}
 	}
 }
