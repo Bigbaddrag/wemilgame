2dc690e350450cc53bc6cf5e52bfe69285bdb274
Bigbaddrag <sephirothpw@hotmail.com>
2016-08-29 21:25:34 -0500

    Got a character controller
    slopes and one way platforms are a thing now

---
+++ WemilGame/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using Prime31;
+
+
+public class CameraFollow : MonoBehaviour
+{
+	public Transform target;
+	public float smoothDampTime = 0.2f;
+	[HideInInspector]
+	public new Transform transform;
+	public Vector3 cameraOffset;
+	public bool useFixedUpdate = false;
+
+	private Controller2D _playerController;
+	private Vector3 _smoothDampVelocity;
+
+
+	void Awake()
+	{
+		transform = gameObject.transform;
+		_playerController = target.GetComponent<Controller2D>();
+	}
+
+
+	void LateUpdate()
+	{
+		if( !useFixedUpdate )
+			updateCameraPosition();
+	}
+
+
+	void FixedUpdate()
+	{
+		if( useFixedUpdate )
+			updateCameraPosition();
+	}
+
+
+	void updateCameraPosition()
+	{
+		if( _playerController == null )
+		{
+			transform.position = Vector3.SmoothDamp( transform.position, target.position - cameraOffset, ref _smoothDampVelocity, smoothDampTime );
+			return;
+		}
+
+		if( _playerController.velocity.x > 0 )
+		{
+			transform.position = Vector3.SmoothDamp( transform.position, target.position - cameraOffset, ref _smoothDampVelocity, smoothDampTime );
+		}
+		else
+		{
+			var leftOffset = cameraOffset;
+			leftOffset.x *= -1;
+			transform.position = Vector3.SmoothDamp( transform.position, target.position - leftOffset, ref _smoothDampVelocity, smoothDampTime );
+		}
+	}
+
+}
